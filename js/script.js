91aba3fe707eb0192e0394fe94a0916ec30de3fd
SoumyaPodha <67249492+soumyapodha@users.noreply.github.com>
2021-12-22 12:00:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const firstNames = ["Manish", "Soumya", "Sangita", "Binaya", "Sunita", "Arpit", "Golu", "Girgit", "Ankit", "Suman", "Manas", "Manisha", "Alok","Sumit", "Raj", "Suneana", "Mantu", "Raju", "Balbir","Bikram"];
+const firstNames = ["Manish", "Soumya", "Sangita", "Binaya", "Sunita", "Arpit", "Golu", "Girgit", "Ankit", "Suman", "Manas", "Manisha", "Alok","Sumit", "Raj", "Suneana", "Mantu", "Raju", "Balbir","Bikram","Subasish","Amarendra","nupur","Debdash"];
 
 const secondNames = ["Lisa", "Biki", "Lima", "Avijit", "Kamal", "Pushpita", "Sagarika", "Sanjat", "Anand", "Chinu", "Alibha", "Subhankar", "Roji", "Satish", "Sanket", "Subham", "Subhankar", "Sudam", "Jogesh", "Shibun","Rakesh","Jaishankar", "Pinku"];
 
